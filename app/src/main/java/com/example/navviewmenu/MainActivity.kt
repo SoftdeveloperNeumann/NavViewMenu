c4c642dfb5846d5516273ce78f3023ac2e4a982a
Frank <f.neumann.berlin@googlemail.com>
2023-01-24 11:22:38 +0100

    fertiges layout

--- app/src/main/java/com/example/navviewmenu/MainActivity.kt
+++ app/src/main/java/com/example/navviewmenu/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.navviewmenu
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.navviewmenu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toolbar.title = "NavViewApp"
+        binding.toolbar.setTitleTextColor(Color.WHITE)
     }
 }
